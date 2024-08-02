3b26ab51ebeb8096925eb33f0e5ff78db141f296
AlanX404 <76613101+alanwildan@users.noreply.github.com>
2024-08-03 02:34:54 +0700

    Create Sabtu.jsx

---
+++ src/components/Mapel/Sabtu.jsx
@@ -0,0 +1,48 @@
+const Sabtu = () => {
+    return (
+        <>
+            <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
+                <div className="w-[50%] text-base font-medium">Ta'lim</div>
+                <div className="flex justify-center items-center text-sm">06:45-07:30</div>
+            </div>
+            <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
+                <div className="w-[50%] text-base font-medium">Biologi</div>
+                <div className="flex justify-center items-center text-sm">07:30-08:15</div>
+            </div>
+            <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
+                <div className="w-[50%] text-base font-medium">Fisika</div>
+                <div className="flex justify-center items-center text-sm">08:15-09:00</div>
+            </div>
+            <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
+                <div className="w-[50%] text-base font-medium">Fisika</div>
+                <div className="flex justify-center items-center text-sm">09:00-09:45</div>
+            </div>
+            <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
+                <div className="w-[50%] text-base font-medium">Istirahat</div>
+                <div className="flex justify-center items-center text-sm">09:45-10:05</div>
+            </div>
+            <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
+                <div className="w-[50%] text-base font-medium">Jepang</div>
+                <div className="flex justify-center items-center text-sm">10:05-10:50</div>
+            </div>
+            <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
+                <div className="w-[50%] text-base font-medium">Informatika</div>
+                <div className="flex justify-center items-center text-sm">10:50-11:35</div>
+            </div>
+            <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
+                <div className="w-[50%] text-base font-medium">Qurdis</div>
+                <div className="flex justify-center items-center text-sm">11:35-12:20</div>
+            </div>
+            <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
+                <div className="w-[50%] text-base font-medium">Fikih</div>
+                <div className="flex justify-center items-center text-sm">12:20-13:05</div>
+            </div>
+            <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="900">
+                <div className="w-[50%] text-base font-medium">Fikih</div>
+                <div className="flex justify-center items-center text-sm">13:05-13:35</div>
+            </div>
+        </>
+    )
+}
+
+export default Sabtu;
