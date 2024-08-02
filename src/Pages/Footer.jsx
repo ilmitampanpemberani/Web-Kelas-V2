7c4cf1a16274362a25cf6959cf4e519a6902292d
AlanX404 <76613101+alanwildan@users.noreply.github.com>
2024-08-03 00:52:04 +0700

    bwh

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -20,13 +20,8 @@ const Footer = () => {
 							<h2 class="mb-4 text-sm font-semibold uppercase text-white">CREATED BY</h2>
 							<ul class="text-gray-400 dark:text-gray-400 font-medium">
 								<li class="mb-2">
-									<a href="https://www.instagram.com/ekizr_/?hl=id" class="hover:underline">
-										EKIZR
-									</a>
-								</li>
-								<li>
-									<a href="https://www.instagram.com/dafyall/?hl=id" class="hover:underline">
-										DAFYALL
+									<a href="http://alwil-x.rf.gd" class="hover:underline">
+										Alnn
 									</a>
 								</li>
 							</ul>
@@ -35,13 +30,8 @@ const Footer = () => {
 							<h2 class="mb-4 text-sm font-semibold uppercase text-white">Follow us</h2>
 							<ul class="text-gray-400 dark:text-gray-400 font-medium">
 								<li class="mb-2">
-									<a href="https://www.instagram.com/acetkjthree/?hl=id" class="hover:underline ">
+									<a href="https://www.instagram.com/unescoxiia/?hl=id" class="hover:underline ">
 										Instagram
-									</a>
-								</li>
-								<li>
-									<a href="https://www.youtube.com/@11TKJ3" class="hover:underline">
-										Youtube
 									</a>
 								</li>
 							</ul>
@@ -60,7 +50,7 @@ const Footer = () => {
 				<div class="sm:flex sm:items-center sm:justify-between">
 					<div class="flex mt-4 justify-center sm:mt-0">
 						<p className="text-[0.7rem] text-white opacity-70">
-							© {new Date().getFullYear()} Kelas XII TKJ 3 | Di Kelola Oleh Siswa TKJ 3
+							© {new Date().getFullYear()} Kelas XII IPA 2 | Di Kelola Oleh ALAN
 						</p>
 					</div>
 				</div>
