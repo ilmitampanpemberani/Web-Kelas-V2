ddd6a32e768751f6f08f9e225df37c410976443d
AlanX404 <76613101+alanwildan@users.noreply.github.com>
2024-08-05 12:26:24 +0700

    Update Rabu.jsx

--- src/components/Mapel/Rabu.jsx
+++ src/components/Mapel/Rabu.jsx
@@ -6,15 +6,15 @@ const Rabu = () => {
                 <div className="flex justify-center items-center text-sm">06:45-07:30</div>
             </div>
             <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
-                <div className="w-[50%] text-base font-medium">Jawa</div>
+                <div className="w-[50%] text-base font-medium">Bahasa Jawa</div>
                 <div className="flex justify-center items-center text-sm">07:30-08:15</div>
             </div>
             <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
-                <div className="w-[50%] text-base font-medium">Indo</div>
+                <div className="w-[50%] text-base font-medium">Bahasa Indonesia</div>
                 <div className="flex justify-center items-center text-sm">08:15-09:00</div>
             </div>
             <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
-                <div className="w-[50%] text-base font-medium">Indo</div>
+                <div className="w-[50%] text-base font-medium">Bahasa Indonesia</div>
                 <div className="flex justify-center items-center text-sm">09:00-09:45</div>
             </div>
             <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
@@ -22,7 +22,7 @@ const Rabu = () => {
                 <div className="flex justify-center items-center text-sm">09:45-10:05</div>
             </div>
             <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
-                <div className="w-[50%] text-base font-medium">Jepang</div>
+                <div className="w-[50%] text-base font-medium">Bahasa Jepang</div>
                 <div className="flex justify-center items-center text-sm">10:05-10:50</div>
             </div>
             <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
@@ -34,7 +34,7 @@ const Rabu = () => {
                 <div className="flex justify-center items-center text-sm">11:35-12:20</div>
             </div>
             <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="800">
-                <div className="w-[50%] text-base font-medium">PP</div>
+                <div className="w-[50%] text-base font-medium">PPKN</div>
                 <div className="flex justify-center items-center text-sm">12:20-13:05</div>
             </div>
             <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="900">
