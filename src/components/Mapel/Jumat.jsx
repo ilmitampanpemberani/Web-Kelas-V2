21bb47451bebc7ab7bd6cb7a4560ffc272933213
AlanX404 <76613101+alanwildan@users.noreply.github.com>
2024-08-05 12:22:27 +0700

    Update Jumat.jsx

--- src/components/Mapel/Jumat.jsx
+++ src/components/Mapel/Jumat.jsx
@@ -1,29 +1,36 @@
 const Jumat = () => {
-	return (
-		<>
-			<div
-				className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
-				data-aos="fade-up"
-				data-aos-duration="600">
-				<div className="w-[50%] text-base  font-medium">TKJ</div>
-				<div className="flex justify-center items-center text-sm">07.00-09.40</div>
-			</div>
-			<div
-				className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60"
-				data-aos="fade-up"
-				data-aos-duration="600">
-				<div className="w-[50%] text-base  font-medium">Istirahat</div>
-				<div className="flex justify-center items-center text-sm">09.40-10.20</div>
-			</div>
-			<div
-				className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
-				data-aos="fade-up"
-				data-aos-duration="700">
-				<div className="w-[50%] text-base  font-medium">PPKn</div>
-				<div className="flex justify-center items-center text-sm">09.40-11.40</div>
-			</div>
-		</>
-	)
+    return (
+        <>
+            <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
+                <div className="w-[50%] text-base font-medium">Biologi</div>
+                <div className="flex justify-center items-center text-sm">06:30-07:00</div>
+            </div>
+            <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
+                <div className="w-[50%] text-base font-medium">Biologi</div>
+                <div className="flex justify-center items-center text-sm">07:00-07:30</div>
+            </div>
+            <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
+                <div className="w-[50%] text-base font-medium">Indo</div>
+                <div className="flex justify-center items-center text-sm">07:30-08:00</div>
+            </div>
+            <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
+                <div className="w-[50%] text-base font-medium">Indo</div>
+                <div className="flex justify-center items-center text-sm">08:00-08:30</div>
+            </div>
+            <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
+                <div className="w-[50%] text-base font-medium">Istirahat</div>
+                <div className="flex justify-center items-center text-sm">08:30-08:50</div>
+            </div>
+            <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
+                <div className="w-[50%] text-base font-medium">Arab</div>
+                <div className="flex justify-center items-center text-sm">08:50-09:20</div>
+            </div>
+            <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
+                <div className="w-[50%] text-base font-medium">Arab</div>
+                <div className="flex justify-center items-center text-sm">09:20-09:50</div>
+            </div>
+        </>
+    )
 }
 
-export default Jumat
+export default Jumat;
