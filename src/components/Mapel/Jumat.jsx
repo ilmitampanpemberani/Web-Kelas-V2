574a13e0791bd7af28dadd839bfce98b644d9903
AlanX404 <76613101+alanwildan@users.noreply.github.com>
2024-08-05 12:24:28 +0700

    Update Jumat.jsx

--- src/components/Mapel/Jumat.jsx
+++ src/components/Mapel/Jumat.jsx
@@ -10,11 +10,11 @@ const Jumat = () => {
                 <div className="flex justify-center items-center text-sm">07:00-07:30</div>
             </div>
             <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
-                <div className="w-[50%] text-base font-medium">Indo</div>
+                <div className="w-[50%] text-base font-medium">Bahasa Indonesia</div>
                 <div className="flex justify-center items-center text-sm">07:30-08:00</div>
             </div>
             <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
-                <div className="w-[50%] text-base font-medium">Indo</div>
+                <div className="w-[50%] text-base font-medium">Bahasa Indonesia</div>
                 <div className="flex justify-center items-center text-sm">08:00-08:30</div>
             </div>
             <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
@@ -22,11 +22,11 @@ const Jumat = () => {
                 <div className="flex justify-center items-center text-sm">08:30-08:50</div>
             </div>
             <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
-                <div className="w-[50%] text-base font-medium">Arab</div>
+                <div className="w-[50%] text-base font-medium">Bahasa Arab</div>
                 <div className="flex justify-center items-center text-sm">08:50-09:20</div>
             </div>
             <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
-                <div className="w-[50%] text-base font-medium">Arab</div>
+                <div className="w-[50%] text-base font-medium">Bahasa Arab</div>
                 <div className="flex justify-center items-center text-sm">09:20-09:50</div>
             </div>
         </>
