aa09ce81bf4f4eb175766d7a6a859c570b6d7ffa
AlanX404 <76613101+alanwildan@users.noreply.github.com>
2024-08-05 12:27:17 +0700

    Update Selasa.jsx

--- src/components/Mapel/Selasa.jsx
+++ src/components/Mapel/Selasa.jsx
@@ -2,19 +2,19 @@ const Selasa = () => {
     return (
         <>
             <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
-                <div className="w-[50%] text-base font-medium">Matwa</div>
+                <div className="w-[50%] text-base font-medium">Matematika Wajib</div>
                 <div className="flex justify-center items-center text-sm">06:45-07:30</div>
             </div>
             <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
-                <div className="w-[50%] text-base font-medium">Matwa</div>
+                <div className="w-[50%] text-base font-medium">Matematika Wajib</div>
                 <div className="flex justify-center items-center text-sm">07:30-08:15</div>
             </div>
             <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
-                <div className="w-[50%] text-base font-medium">Aqidah</div>
+                <div className="w-[50%] text-base font-medium">Akidah Akhlak</div>
                 <div className="flex justify-center items-center text-sm">08:15-09:00</div>
             </div>
             <div className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="600">
-                <div className="w-[50%] text-base font-medium">Aqidah</div>
+                <div className="w-[50%] text-base font-medium">Akidah Akhlak</div>
                 <div className="flex justify-center items-center text-sm">09:00-09:45</div>
             </div>
             <div className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3" data-aos="fade-up" data-aos-duration="700">
